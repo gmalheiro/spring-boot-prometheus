5e9802d574809f423c7a051b224369ecc7dcbfc8
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2025-01-20 08:41:13 -0300

    feat: add spring project

---
+++ grafana-prometheus-springboot/src/main/java/br/com/gmalheiro/grafana_prometheus_springboot/GrafanaPrometheusSpringbootApplication.java
@@ -0,0 +1,13 @@
+package br.com.gmalheiro.grafana_prometheus_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrafanaPrometheusSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrafanaPrometheusSpringbootApplication.class, args);
+	}
+
+}
---
+++ grafana-prometheus-springboot/src/test/java/br/com/gmalheiro/grafana_prometheus_springboot/GrafanaPrometheusSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gmalheiro.grafana_prometheus_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrafanaPrometheusSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
